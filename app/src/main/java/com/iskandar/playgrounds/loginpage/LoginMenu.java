d4f2562fc128f1a10436094b3d7671ad353d635b
Junesjhz <cleadeus@outlook.com>
2021-03-25 17:25:19 +0700

    new login menu, connect it to firebase later for authentication

---
+++ app/src/main/java/com/iskandar/playgrounds/loginpage/LoginMenu.java
@@ -0,0 +1,16 @@
+package com.iskandar.playgrounds.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.iskandar.playgrounds.R;
+
+public class LoginMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_menu);
+    }
+}
---
+++ app/src/main/java/com/iskandar/playgrounds/loginpage/SignupMenu.java
@@ -0,0 +1,16 @@
+package com.iskandar.playgrounds.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.iskandar.playgrounds.R;
+
+public class SignupMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_menu);
+    }
+}
